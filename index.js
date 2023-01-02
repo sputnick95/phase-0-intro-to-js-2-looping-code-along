a5c5d60b7f6278a7116a0b5634459f7399944cbb
Nicolas Sapountzis <nsap95@gmail.com>
2023-01-02 10:47:25 +0700

    commit tree e7a820c70a10952b7bd6a8ba1aa79ddd12cc90f8

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const deck = ["Guadalupe", "Ollie", "Aki"]
+const deck2 =[]
+
+function writeCards(cards) {
+    for (let i=0; i<deck.length; i++) {
+        deck2.push(`Thank you, ${deck[i]}, for the wonderful surprise gift!`)
+    }
+    return deck2
+}
+
+function countDown(num) {
+    let i = 10;
+    while (i>=0) {
+        console.log(i)
+        --i
+    }
+}
